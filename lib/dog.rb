68ff91c776b5d121e170c461af135b847cfb79fa
Owen Mowery <owenmowery99@gmail.com>
2021-03-04 17:19:47 -0500

    commit tree 5c61ed4868a4b6271e33e0c1994efb9d64b36211

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+class Dog
+
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each do |instance|
+            puts instance.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+
+end
